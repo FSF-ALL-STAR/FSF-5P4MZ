17992bda29ff6d2e8bcdd967f7f20f999f3096a1
FSF-ALL-STAR <39660261+FSF-ALL-STAR@users.noreply.github.com>
2018-05-27 00:28:46 +0000

    Add files via upload
    This is Spammer For kebahagian :) :D

---
+++ 4.php
@@ -0,0 +1,20 @@
+﻿<?php
+include 'bomphd.php';
+
+
+/*
+    ©INDOFams StarFive All Star
+*/
+
+$init = new Bom();
+
+//Eksekusi Sms Boomber
+echo "Nomor Target (pakai 62): ";
+$a = trim(fgets(STDIN));
+$init->no = "$a";
+echo "Jumlah Pesan: ";
+$b = trim(fgets(STDIN));
+$loop = "$b";
+for ($i=0; $i < $loop; $i++) {
+    $init->Verif($init->no);
+}
---
+++ FSF-5P4MZ.sh
@@ -0,0 +1,83 @@
+﻿--#!/system/xbin/bash
+clear
+toilet -f slant --gay "FSF-SPAM"
+sleep 1
+echo "\033[31;1m Macam-macam tool untuk spam"
+sleep 1
+echo "\033[33;1m Cracker: FSF All Star [iL MmaaiilL]"
+sleep 1
+echo "\033[32;1m ★contack: +083808068611"
+sleep 1
+echo "\033[34;1m ★INDOFams StarFive All Star"
+sleep 1
+echo "\033[36;1m Spam Yang Tersedia :"
+echo "1. Tokopedia"
+echo "2. Telkomsel"
+echo "3. Matahari Mall"
+echo "4. PHD"
+echo "5. Jd.Id"
+echo "6. Email Bomber"
+echo "0. Cabut"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[34;1m"
+figlet "tokopedia"
+php 1.php
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+php 2.php
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[31;1m"
+figlet "Mthr Mall"
+php 3.php
+fi
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono12 -F gay "PHD"
+echo "\033[30;1m"
+php 4.php
+fi
+
+if
+[ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f mono12 -F gay "Jd.Id"
+echo "\033[33;1m"
+php 5.php
+fi
+
+if
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f standard -F gay "Email Bomber"
+echo "\033[36;1m"
+python2 6.py
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Semoga kamu puas dan bahagia :) :D"
+sleep 1
+fi
+
